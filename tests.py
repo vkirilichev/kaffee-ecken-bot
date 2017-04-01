41a9e08e00e699c18226bc0731203102142eadb3
Vasily Kirilichev <vkirilichev@gmail.com>
2017-04-01 23:51:02 +0200

    add travis ci

---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class BotTest(unittest.TestCase):
+    def dummy(self):
+        self.assertEqual(1, 1)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
